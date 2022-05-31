83316f4ebc04e48fc4c7546a8014c37664992d0d
Otavio <otaviorodriguesv@hotmail.com>
2022-05-31 17:07:24 -0300

    first commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 ﻿import axios from "axios";
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://maistocadasufmg-bcsth.ondigitalocean.app/backmaistocadasufmg/apiS",//"https://maistocadasufmg.herokuapp.com/api", //"http://localhost:8080/api",
     headers: {
         "Content-type": "application/json"
     }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "./responsive.css";
+import "./App.css";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
